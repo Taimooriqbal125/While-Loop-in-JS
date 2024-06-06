d21a97a847620c4372aa381932f6e3b5e8ae523c
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-06 03:38:38 -0700

    While loop in JS

---
+++ script.js
@@ -0,0 +1,35 @@
+// While loop
+// Any loop have the following things to do work
+// Initial value
+// Condition
+// Increnment/Decrenment
+
+var a = 1;
+var i = 0;
+while (i<100) {
+    console.log(i+a)
+    i++
+}
+
+var i = 11
+while(i > 0 ){
+    console.log(i-1)
+    i--
+}
+
+var hello = "hello world";
+var t = 0;
+while(t < 5){
+    console.log(hello)
+    t++
+}
+
+// var d = 0;
+for (let d = 0; d < 10 ; d++) {
+   
+    console.log(hello)
+}
+var f = 0;
+for(var f = 0;f < 100; f++ ){
+    console.log("numbers", f)
+}
