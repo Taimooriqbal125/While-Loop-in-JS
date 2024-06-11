24f653e3f6c1b52854771d117e0d99040f32bc5c
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-06-11 04:59:07 -0700

    While

--- script.js
+++ script.js
@@ -4,32 +4,37 @@
 // Condition
 // Increnment/Decrenment
 
-var a = 1;
-var i = 0;
-while (i<100) {
-    console.log(i+a)
-    i++
-}
+// var a = 1;
+// var i = 0;
+// while (i<100) {
+//     console.log(i+a)
+//     i++
+// }
 
-var i = 11
-while(i > 0 ){
-    console.log(i-1)
-    i--
-}
+// var i = 11
+// while(i > 0 ){
+//     console.log(i-1)
+//     i--
+// }
 
-var hello = "hello world";
-var t = 0;
-while(t < 5){
-    console.log(hello)
-    t++
-}
+// var hello = "hello world";
+// var t = 0;
+// while(t < 5){
+//     console.log(hello)
+//     t++
+// }
 
-// var d = 0;
-for (let d = 0; d < 10 ; d++) {
+// // var d = 0;
+// for (let d = 0; d < 10 ; d++) {
    
-    console.log(hello)
-}
-var f = 0;
-for(var f = 0;f < 100; f++ ){
-    console.log("numbers", f)
+//     console.log(hello)
+// }
+// var f = 0;
+// for(var f = 0;f < 100; f++ ){
+//     console.log("numbers", f)
+// }
+
+
+for ( var i = 0 ; i<0; i++){
+    console.log("hi")
 }
